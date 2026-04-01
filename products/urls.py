b646a0302d1225a8640f2ca4a6a8b8c13c5a6ac1
Ahm455 <Ahmedgoharkiyani@gmail.com>
2026-04-01 16:45:59 +0500

    improving model and using generic class views

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ app_name = 'products'
 urlpatterns = [ 
     path('add/', add_medicine.as_view(), name='add_medicine'),
     path('list/', medicine_list.as_view(), name='medicine_list'), 
-    path('add_stock/', add_stock.as_view(), name='add_stock')  
+    path('add-stock/<int:pk>/', add_stock.as_view(), name='add_stock')
 ]
--- products/views.py
+++ products/views.py
@@ -1,14 +1,21 @@
 from .models import Medicine
-from django.views.generic import ListView ,CreateView 
+from django.urls import reverse_lazy
+from django.views.generic import ListView ,CreateView ,UpdateView
 
 class add_medicine(CreateView):
     model = Medicine
     fields = '__all__'
 
-class add_stock(CreateView):
+class add_stock(UpdateView):
     model = Medicine
-    fields = ['name','quantity']  
-    template_name = "products/add_stock.html" 
+    fields = ['name','quantity']
+    template_name = "products/add_stock.html"
+    success_url = reverse_lazy('products:medicine_list')
+ 
+    def form_valid(self, form):
+        added_qty = form.cleaned_data['quantity']
+        form.instance.quantity = form.instance.quantity + added_qty
+        return super().form_valid(form)
 
 class medicine_list(ListView):
     model = Medicine
